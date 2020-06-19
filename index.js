aeb3b2f8c9fc0ea7252edb89639ae566d2cd9ae3
fcasaloti <fcasaloti@hotmail.com>
2020-06-19 00:04:18 -0700

    Last Change before submit

---
+++ index.js
@@ -0,0 +1,50 @@
+//Set the utilization of the modules and constants
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require('fs');
+const bodyParser = require("body-parser");
+
+//Set the use of some functionalities
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
+
+//Open the JSON database file
+let rawdata = fs.readFileSync('database.json');
+let database = JSON.parse(rawdata);
+
+//Managing POST request
+app.post("/", (req, res) => {
+    console.log(req.body); //debug
+    const page_id = req.body.page_id;
+    const result = getStudents(page_id);
+    res.contentType('Content-Type', 'application/json');
+    res.status(200).json(result);
+});
+
+//Router to the root folder
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+//Get the list of Students per page from the JSON object
+function getStudents(page_id) {
+    let studentsList = [];
+    let i = 0;
+    let row = page_id * 10;
+
+    while (i < 10) {
+        let indexNum = row + i;
+
+        if (indexNum < database.students.length)
+            studentsList[i] = database.students[indexNum];
+        i++;
+    }
+    studentsList.push(Math.ceil(database.students.length / 10));
+    return studentsList;
+};
+
+//Listening port
+app.listen(port, () => console.log(`Listening port ${port}`));
--- js/script.js
+++ js/script.js
@@ -1,53 +1,82 @@
-//Declare constants and import JSON file that is sent from server
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){};
-xhr.open('GET','stuBackList.json');
-xhr.send();
-
-//Function to convert data from JSON
-xhr.onload=function(){
-  jsonList=JSON.parse(xhr.responseText);
-  JSON.stringify(jsonList);
-
-  printValue(jsonList);
-
-  printPagesNum(jsonList)
-};
-
-//Function to write Students Name dynamically
-function printValue(json){
-  var studentPage = 
-  `<div class="page-header cf">
-  <h2>Students</h2>
-  </div><ul class="student-list">`;
-
-    for (let i = 0; i < json.length - 1; i++){
-
-     studentPage += 
-    `<li class="student-item cf">
-    <div class="student-details">
-        <img class="avatar" src=${json[i].avatar}>
-        <h3>${json[i].name}</h3>
-        <span class="email">${json[i].email}</span>
-    </div>
-    <div class="joined-details">
-          <span class="date">${json[i].joined}</span>
-    </div>
-    </li>`
-    }
-    studentPage += `</ul></div>`;
-    document.getElementsByClassName("page-header cf")[0].innerHTML = studentPage;
+// Onload call
+window.onload = function () {
+    loadPageContent(0);
 }
 
-//Function to print numbers of pages dynamically
-function printPagesNum(json){
-  var student = `<div class="pagination">
-  <ul>`;
+// Loads Page Content: Body and Pagination
+function loadPageContent(page_id) {
+    getStudents(page_id)
+        .then(function (data) {
+            setBody(data);
+            setPagination(data, page_id);
+        });
+}
 
-    for (let i = 1; i <= (json[json.length - 1]) ; i++){
-      student += `<li><form method="POST" action="/"><input type="hidden" id="${i}" name="data" value="${i - 1}">${i}
-      <button type="submit" id="${i}" name="submit"></button></form></li>`;
+function getStudents(page_id) {
+    let promise = new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        let data = JSON.stringify({ "page_id": page_id });
+        request.addEventListener("readystatechange", function () {
+            if (this.readyState !== 4) return;
+
+            if (this.readyState === 4 && this.status == 200) {
+                console.log(JSON.parse(this.responseText)); // Debug
+                resolve(JSON.parse(this.responseText));
+            }
+            else {
+                reject({
+                    status: this.status
+                });
+            }
+        });
+
+        request.open('POST', 'http://localhost:3000');
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(data);
+    });
+    return promise;
+}
+
+//Function to write Students Name dynamically
+function setBody(data) {
+    if (typeof data !== 'undefined') {
+        let studentPage =
+            `<div class="page-header cf">
+              <h2>Students</h2>
+          </div>
+          <ul class="student-list">`;
+
+        for (let i = 0; i < data.length - 1; i++) {
+            studentPage +=
+                `<li class="student-item cf">
+                  <div class="student-details">
+                      <img class="avatar" src=${data[i].avatar}>
+                      <h3>${data[i].name}</h3>
+                      <span class="email">${data[i].email}</span>
+                  </div>
+                  <div class="joined-details">
+                      <span class="date">${data[i].joined}</span>
+                  </div>
+              </li>`
+        }
+        studentPage += `</ul></div>`;
+        document.getElementsByClassName("page-header cf")[0].innerHTML = studentPage;
     }
-    student += `</ul></div>`;
-    document.getElementsByClassName("pagination")[0].innerHTML = student;
+}
+
+// Function to print numbers of pages dynamically
+function setPagination(data, page_id) {
+    if (typeof data !== 'undefined') {
+        let student = `<div class="pagination">
+          <ul id="pagination">`;
+
+        for (let i = 1; i <= (data[data.length - 1]); i++) {
+            let active = (page_id + 1 == i ? 'active' : '');
+            student += `<li>
+              <a class="${active}" onclick="loadPageContent(${i - 1})">${i}</a>
+          </li>`;
+        }
+        student += `</ul></div>`;
+        document.getElementsByClassName("pagination")[0].innerHTML = student;
+    }
 }
